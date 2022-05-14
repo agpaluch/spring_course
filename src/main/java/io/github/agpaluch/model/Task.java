2f619d78b56d4fdf2929712f8bd04deccb1efcdb
Agnieszka <agnieszka.paluch90@gmail.com>
2022-05-14 15:55:16 +0200

    Task model and RepositoryRestResource

---
+++ src/main/java/io/github/agpaluch/model/Task.java
@@ -0,0 +1,40 @@
+package io.github.agpaluch.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="tasks")
+class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String description;
+    private boolean done;
+
+    Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    void setDescription(final String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    void setDone(final boolean done) {
+        this.done = done;
+    }
+}
